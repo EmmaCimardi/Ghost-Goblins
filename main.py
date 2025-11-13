0f65708c5c3cfd4e2c5a7d0a355a1208badce4ea
RimMadnas <rimmadnas@gmail.com>
2025-11-13 12:36:15 +0100

    prova2

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import actor
 g2d.init_canvas((600,250))
 g2d.draw_image("ghosts-goblins-bg.png", (0,0), (0,0)) #sfondo
 #ciao emma maria perla 
+actor.create_hero((50,150))
+#ciao emma maria perla
 g2d.main_loop()
 
 
