cc5ff5dd09f155c85bfacd8f6dafbee5d1ab9ae2
EmmaCimardi <empcc23@gmail.com>
2025-11-14 14:17:16 +0100

    Arthur r zombie main

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 import g2d
 from actor import Actor, Point, Arena
 #ciaoooooooo
-g2d.init_canvas((600, 250))
-g2d.draw_image("ghosts-goblins-bg.png", (0, 0))
 
 class Arthur(Actor):
-    def __init__(self, x, y):
-        self._x = x     #pos iniziale art
-        self._y = y     #pos iniziale art
-        self._w = 32    #larghezza art
-        self._h = 48    #altezza art
+    def __init__(self, pos):
+             #pos iniziale art
+        self._x, self._y = pos    #pos iniziale art
+        self._w = 29    #larghezza art
+        self._h = 31    #altezza art
         self._speed = 3 #velocita orizzontale 
         self._vy = 0    #velocita verticale
         self._direction = 1 
@@ -25,14 +23,15 @@ class Arthur(Actor):
         if "w" in keys and not self._jumping:  # "w", art salta .
             self._vy = -10
             self._jumping = True
+            
         
         # Gravità
         self._vy += 0.5
         self._y += self._vy
         
         # collisione con il terreno 
-        if self._y > 200:
-            self._y = 200
+        if self._y > 180: #uguale a self._y
+            self._y = 180 #mofdifica altezza di arthur nel canva
             self._vy = 0
             self._jumping = False
         
@@ -49,24 +48,7 @@ class Arthur(Actor):
         return (self._w, self._h)
     
     def sprite(self):
-        return None
-
-arena = Arena((600, 260))
-arthur = Arthur(0, 200)
-arena.spawn(arthur)
-
-def main():
-    g2d.clear_canvas()
-    g2d.draw_image("ghosts-goblins-bg.png", (0, 0))
-    
-    x, y = arthur.pos()
-    w, h = arthur.size()
-    g2d.draw_rect((x, y), (w, h))  # quadrato rosso
-    
-    arena.tick(g2d.current_keys())
-
-g2d.main_loop(main)
-
+        return 128,610 # x_sprite,y_sprite
 
 #Classe zombie
 
@@ -78,13 +60,14 @@ class Zombie(Actor):
         self._dx, self._dy = self._speed, self._speed
         
     def move(self, arena: Arena):
-        for other in arena.collisions():
-            if not isinstance(other, ):
-                x, y = other.pos()
-                if self._x + self._dx < 0:
-                    self._dx = self._speed
-                else:
-                    self._dx = -self._speed
+       
+       #for other in arena.collisions():
+            #if not isinstance(other, ):
+             #   x, y = other.pos()
+              #  if self._x + self._dx < 0:
+               #     self._dx = self._speed
+                #else:
+                 #   self._dx = -self._speed""
                 
 
         arena_w, arena_h = arena.size()
@@ -122,12 +105,21 @@ def tick():
 
             
 def main():
+    #ARENA
     global arena
-    
     arena = Arena((600, 260)) #dim arena
-    arena.spawn(Zombie((0,200))) #spawn zombie che vanno da destra a sinistra
-     
     g2d.init_canvas(arena.size()) #creo canvas
+    
+    #ZOMBIE
+    arena.spawn(Zombie((0,180))) #spawn zombie che vanno da destra a sinistra
+     
+    #ARTHUR
+    arena.spawn(Arthur((0, 150)))
+
+    arena.tick(g2d.current_keys())
+    
+    #CANVAS
+    
     g2d.main_loop(tick)
     
 
